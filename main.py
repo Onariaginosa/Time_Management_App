6babbfe87ff052fbc1a7dba91b731d1c2e852f1a
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-13 15:37:58 -0700

    working code finally

--- main.py
+++ main.py
@@ -9,7 +9,30 @@ jinja_env = jinja2.Environment(
    extensions=['jinja2.ext.autoescape'],
    autoescape=True)
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Welcome to Toki Adventure")
 
-   app = webapp2.WSGIApplication([
+class Character(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Choose Your character party ppl")
+
+class DogStory(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Story goes here for Dog")
+
+class PandaStory(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Story goes here for Panda")
+class CatStory(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Story goes here for Cat")
+
+
+app = webapp2.WSGIApplication([
    ('/', MainPage),
+   ('/character',Character),
+   ('/dog',DogStory),
+   ('/panda',PandaStory),
+   ('/cat',CatStory)
 ], debug=True)
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -3,7 +3,7 @@
 
 //var button =
 //document.createUlement("button");
-button.innerHTML = Start Your Adventure;
+//button.innerHTML = Start Your Adventure;
 
 //button.forEach(item)
 //function pressButton(event) {
