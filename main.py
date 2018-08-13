e43d7848d65b3f5bc64aae61ba3b3adfabbcad70
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-13 10:58:15 -0700

    yo

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
  #this is a test
-print("tester code")
+import webapp2
+from random import shuffle
+import os
+import jinja2
+
+jinja_env = jinja2.Environment(
+   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+   extensions=['jinja2.ext.autoescape'],
+   autoescape=True)
+
+
+   app = webapp2.WSGIApplication([
+   ('/', MainPage),
+], debug=True)
