91368078d81f11cf646a3347740b1e72db5b7c05
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-13 12:17:12 -0700

    broken html but i add some changes/files

---
+++ scripts/welcome.js
@@ -0,0 +1,11 @@
+//const option = document.querySelectorAll("Yes");
+
+
+//var button =
+//document.createUlement("button");
+button.innerHTML = Start Your Adventure;
+
+//button.forEach(item)
+//function pressButton(event) {
+
+//}
