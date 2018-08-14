2dac512f8d980a394e493eb544f908e704aa3cf8
Onariaginosa Igbinedion <Ona.Igbinedion@gmail.com>
2018-08-14 09:47:09 -0700

    Kitty loop added AUGHHHHHHHHHHHHHHgit add .git add .git add .git add .git add .! AHHHHHHHHH

--- seed_data.py
+++ seed_data.py
@@ -1,4 +1,4 @@
-from models import Character, KBorn, PB2, PPath, Path
+from models import Character, KBorn, PB2, PPath, Path, Looper
 
 
 def seed_data():
@@ -12,28 +12,38 @@ def seed_data():
     born_3 = "You are a tiny teacup kitty born in a litter of nine. You are the runt of the bunch",
     owner = kitty_key).put()
 
+    kitty_post_born = PB2(pb_1 = "Congrats! A streetboy found you and took you home. Do you stay with him or run away",
+    pb_2 = "Congrats! A rich person saw you and just had to have you. Do you stay with this rich person or run",
+    pb_3 = "An old man saw you and took you home with him. Do you stay with him or run",
+    owner = kitty_key).put()
+
+    kitty_loop = Looper(loop_1 = "Ouch! The kid was a sadist, and tortured you until you died.",
+    loop_2 = "You have a new home! However, that rich person is cat obsessive and dresses you in tight clothing. Do you run or stay?",
+    loop_3 = "Noice! The old man wanted  a support animal, so you now live your day to day life well fed and happy. However, the old man died of some unknown illness. The old man left you to his nephew. Do you stay with him or run away?",
+    run = "You ran away.").put()
+
+
+
+
+
+
+
     panda_born = Born(born_1 = "For your whole life you've been on the run from hunters. Your mother and father fell victim to the vicous fur trade in China",
     born_2 = "You were born in the San Diego Zoo where you spend your time posing for selfies with little kids and playing with your siblings",
     born_3 = "The circus means everything to you. It has been your home for the last ten years.",
     owner = panda_key).put()
 
-    doggo_born = Born(born_1 = "You wake up in a dark ravine without any family. You are starving. You scavenge for food but cannot find anything.",
-    born_2 = "You were born into a rich family, but they are never home. Your food is overflowing because your owners do not know how much to feed you. The food looks old now.",
-    born_3 = "Your owner pats your head with affection. He gazes into your eyes and rubs you behind your ears, just the way you like. Your tail wags. You almost forget you both have no house to live in",
-    owner = doggo_key).put()
-    )
-
-
-    kitty_post_born = PB2(pb_1 = "Congrats! A streetboy found you and took you home. Do you stay with him or run away",
-    pb_2 = "Congrats! A rich person saw you and just had to have you. Do you stay with this rich person or run",
-    pb_3 = "An old man saw you and took you home with him. Do you stay with him or run",
-    owner = kitty_key).put()
-    )
-
     panda_post_born = PB2(pb_1 = "You become stuck in a trap! OH NO! Should you stay or escape?",
     pb_2 = "A little kid wanders into the zoo and manages to sneak into the cage. Do you call for attention or keep quiet?",
     pb_3 = "Oh no! Your circus has been disbanded by PETA! Do you join a new circus or start a new life?",
     owner = panda_key).put()
+
+
+    doggo_born = Born(born_1 = "You wake up in a dark ravine without any family. You are starving. You scavenge for food but cannot find anything.",
+    born_2 = "You were born into a rich family, but they are never home. Your food is overflowing because your owners do not know how much to feed you. The food looks old now.",
+    born_3 = "Your owner pats your head with affection. He gazes into your eyes and rubs you behind your ears, just the way you like. Your tail wags. You almost forget you both have no house to live in",
+    owner = doggo_key).put()
+
 
     doggo_post_born = PB2(pb_1 = "You see food on the dirty floor. One is a seemingly moldy dog buscuit and a dead muse lies next to it. Which do you eat?",
     pb_2 = "You don't want to have such a boring, empty life. You want more, so you run away. DO you follow the music playing from a dirty car or run aimlessly?"
