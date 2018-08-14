dc247e6cbc1adf72a1f19bb8b98bfad1df354631
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-13 17:20:16 -0700

    Button works some css added

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,11 +1,1 @@
-const option = document.querySelectorAll("Yes");
 
-
-//var button =
-document.createUlement("button");
-button.innerHTML = Start Your Adventure;
-
-button.forEach(item)
-function pressButton(event) {
-
-}
