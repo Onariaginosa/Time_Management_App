5794dfd1781fcd9662cda1018f567c845367603a
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-08-14 15:53:29 -0700

    all imgaes lonked

--- main.py
+++ main.py
@@ -21,23 +21,23 @@ class Character(webapp2.RequestHandler):
 
 class CatStory(webapp2.RequestHandler):
     def get(self):
-        cat_template = jinja_env.get_template("templates/story.html")
+        cat_template = jinja_env.get_template("templates/cat.html")
         self.response.write(cat_template.render())
 class PandaStory(webapp2.RequestHandler):
     def get(self):
-        panda_template = jinja_env.get_template("templates/story.html")
+        panda_template = jinja_env.get_template("templates/panda.html")
         self.response.write(panda_template.render())
 class DogStory(webapp2.RequestHandler):
     def get(self):
-        dog_template = jinja_env.get_template("templates/story.html")
+        dog_template = jinja_env.get_template("templates/dog.html")
         self.response.write(dog_template.render())
 
 app = webapp2.WSGIApplication([
    ('/', MainPage),
    ('/character',Character),
-   ('/cat_story',CatStory),
-   ('/panda_story',PandaStory),
-   ('/dog_story',DogStory)
+   ('/cat',CatStory),
+   ('/panda',PandaStory),
+   ('/dog',DogStory),
 
 
 ], debug=True)
