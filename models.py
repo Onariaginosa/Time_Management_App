0b44222bff657681d8871fcbf6663537b38042ed
Onariaginosa Igbinedion <Ona.Igbinedion@gmail.com>
2018-08-13 17:02:14 -0700

    more database stuff whhhhyyyyyyy

--- models.py
+++ models.py
@@ -2,4 +2,9 @@ from google.appengine.ext import ndb
 
 class Character(ndb.Model):
     species = ndb.StringProperty(required = True)
-    
+
+class Born(ndb.Model):
+    born_1 = ndb.StringProperty(required = True)
+    born_2 = ndb.StringProperty(required = True)
+    born_3 = ndb.StringProperty(required = True)
+    owner = ndb.KeyProperty(Character)
--- seed_data.py
+++ seed_data.py
@@ -1,5 +1,17 @@
-from models import Character
+from models import Character, KBorn
 
 
 def seed_data():
-    kitty_key = Character(species= "Cat")
+    kitty_key = Character(species= "Cat").put()
+    panda_key = Character(species= "Panda").put()
+    doggo_key = Character(species = "Dog").put()
+
+
+
+    kitty_born = Born(born_1 = "You are a ratty streetcat born in a litter of four. Your mom abandoned you",
+    born_2 = "You are a cute kitty cat born in a litter of six. You are super healthy!",
+    born_3 = "You are a tiny teacup kitty born in a litter of nine. You are the runt of the bunch",
+    owner = kitty_key).put()
+
+    panda_born = Born(born_1 = "For your whole life you've been on the run from hunters. Your mother and father fell victim to the vicous fur trade in China",
+    born_2 = "You were born in the San Diego Zoo where you spend your time posing for selfies with little kids and playing with your siblings")
