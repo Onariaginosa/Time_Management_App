0646bf5efef832060e207a082dc871a6e78a5441
Onariaginosa Igbinedion <Ona.Igbinedion@gmail.com>
2018-08-14 22:19:59 -0700

    kitty born is a go

--- cat.py
+++ cat.py
@@ -8,13 +8,15 @@ jinja_env = jinja2.Environment(
    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
    extensions=['jinja2.ext.autoescape'],
    autoescape=True)
-
+seed_data()
 def kat_born():
-    kitty_born = Born.query().fetch()
+    kitty_key = Character.query(Character.species == "Cat").get()
+    kitty_born = Born.query(Born.owner == kitty_key.key).get()
     born_opt = randrange(1,4)
     if born_opt == 1:
-        return kitty_born
+
+        return kitty_born.born_1
     elif born_opt == 2:
-        return kitty_born
+        return kitty_born.born_2
     elif born_opt == 3:
-        return kitty_born
+        return kitty_born.born_3
